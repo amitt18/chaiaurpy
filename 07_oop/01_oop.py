48cd3efcdf86b169803e3dd93b38d30e39856bec
amitt18 <amitrtiwari500@gmail.com>
2025-08-17 09:58:40 +0530

    Class and Object

--- 07_oop/01_oop.py
+++ 07_oop/01_oop.py
@@ -3,9 +3,14 @@ class Car:
        self.brand = brand
        self.model = model
 
+    def full_name(self):
+        return f"{self.brand}{self.model}"   
+
 
 my_car = Car("BMW","Corolla")
 print(my_car.model)
+print(my_car.full_name())
 my_new_car = Car("Porshe","x56")
 print(my_new_car.brand)
 print(my_new_car.model)
+print(my_new_car.full_name())
